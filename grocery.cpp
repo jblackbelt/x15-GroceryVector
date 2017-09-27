cb1ab6e8f5a1e669fa185bfcf922670049be4f3c
jblackbelt <jwest27.mail.csuchico.edu>
2017-09-27 16:47:06 -0700

    all but remove

--- grocery.cpp
+++ grocery.cpp
@@ -17,7 +17,6 @@ int main()
           cout<<"\n (A)dd an item";
           cout<<"\n (S)how grocery list";
           cout<<"\n (Q)uit";
-          cout<<"\n (R)emove";
           cout<<"\nYour choice (A/S/Q/R): ";
           cin>>input;
           if(input == 'S' || input == 's')
@@ -32,20 +31,9 @@ int main()
               cout << "What item would you like to add to your cart?" << endl;
               string itemName;
               cin >> itemName;
-              list.push_back(itemname);
+              list.push_back(itemName);
               cout << "You added " << list[i] << " to your cart\n";
               i++;
-          }
-          if(input == 'R'  ||  input == 'r')
-          {
-            int num=0;
-            for(int x = 0; x<= list.size()-1 ; x++)
-            {
-                cout << x+1 << ". " << list[x] << endl;
-            }
-            cout << "Which item would you like to remove" << endl;
-            cin >> num;
-            list.erase(num-1);
           }
               if(input == 'Q' || input == 'q')
               {
